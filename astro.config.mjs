e3ed7603d9db1236f76d22a00bfba15a135aa6e0
Audrey981 <blondela1215@gmail.com>
2026-07-21 15:36:31 +0200

    test / dans href avec base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,27 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://paulinerebufat.fr',
+  site: 'https://paulineatelierceramique-commits.github.io',
+  base: '/pauline',
+ 
   integrations: [sitemap()],
 });
+
+
+
+
+
+
+
+// à enlever quand le site sera en ligne sur le domaine définitif
+// @ts-check
+//import { defineConfig } from 'astro/config';
+
+// import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+//export default defineConfig({
+ // site: 'https://paulinerebufat.fr',
+  // base: '/pauline',
+ // integrations: [sitemap()],
+// });
